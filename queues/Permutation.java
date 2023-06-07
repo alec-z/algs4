001368d71243b415786d8c3719a79f8478914253
郑志鹏 <zhengzhipeng4@huawei.com>
2023-06-07 13:31:18 +0800

    queues submit 10, try bonus

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,14 +1,24 @@
 import java.util.NoSuchElementException;
 
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> randomQue = new RandomizedQueue<>();
+        int n = 0;
         while (!StdIn.isEmpty()) {
             String str = StdIn.readString();
-            randomQue.enqueue(str);
+            n++;
+            if (n <= k) {
+                randomQue.enqueue(str);
+            } else {
+                if (StdRandom.bernoulli((double)k / n)) {
+                    randomQue.dequeue();
+                    randomQue.enqueue(str);
+                }
+            }
             
         }
         for (int i = 0; i < k; i++) {
